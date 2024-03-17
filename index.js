a7b32f5cca0e709d2ffef433bf64bb0263ec64b5
Yasvanth-2005 <yasvanthhanumantu1@gmail.com>
2024-03-17 16:34:18 +0530

    notifiys

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import helmet from "helmet";
 
 import userRoutes from "./routes/userRoutes.js";
 import eventRoutes from "./routes/eventRoutes.js";
@@ -15,8 +14,6 @@ import adminRoutes from "./routes/adminRoutes.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
