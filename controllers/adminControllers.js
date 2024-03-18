6f4c24b75ce13d91e911f37b4b4f4ec496e68538
Yasvanth-2005 <yasvanthhanumantu1@gmail.com>
2024-03-18 12:58:46 +0530

    admin

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,5 +1,4 @@
 import Admin from "../models/adminModel.js";
-import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export const adminLogin = async (req, res) => {
@@ -10,9 +9,7 @@ export const adminLogin = async (req, res) => {
     if (!user) {
       return res.status(404).json({ message: "Invalid Credentials" });
     }
-
-    const vPass = await bcrypt.compare(password, user.password);
-    if (!vPass) {
+    if (user.password !== password) {
       return res.status(404).json({ message: "Invalid Credentials" });
     }
 
@@ -33,19 +30,18 @@ export const adminRegister = async (req, res) => {
   const { username, password, role } = req.body;
 
   try {
-    const user = await Admin.findOne({ username });
-    if (user) {
-      return res.status(400).json({ message: "User Already exists" });
+    const existingUser = await Admin.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 12);
     const newUser = await Admin.create({
       username,
       role,
-      password: hashedPassword,
+      password,
     });
 
-    return res.status(200).json({ newUser });
+    return res.status(200).json({ user: newUser });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Internal Server Error" });
