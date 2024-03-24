21d5c270157232a963a3cb66bcaf5270b037c1db
Yasvanth-2005 <yasvanthhanumantu1@gmail.com>
2024-03-24 11:50:57 +0530

    backend

--- controllers/eventControllers.js
+++ controllers/eventControllers.js
@@ -163,7 +163,9 @@ export const eventRegistration = async (req, res) => {
       { $push: { regEvents: eventId } }
     );
 
-    return res.status(200).json({ message: "Registered Successfully" });
+    return res
+      .status(200)
+      .json({ event: updatedEvent, message: "Registered Successfully" });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "Internal Server Error" });
