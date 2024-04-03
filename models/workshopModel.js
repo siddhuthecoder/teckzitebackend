2a2edb0f80518a0a5ff175073c3139b36a228ea1
Yasvanth-2005 <yasvanthhanumantu1@gmail.com>
2024-04-03 18:43:49 +0530

    events

--- models/workshopModel.js
+++ models/workshopModel.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const regStudentsSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
+  email: { type: String },
   name: String,
   phno: String,
   idNumber: String,
